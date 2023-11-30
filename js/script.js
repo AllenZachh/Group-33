4e01015af270f68905f2c1eb7e2fd9e1e4b2ad37
Zakariya-Yaqub <148327192+Zakariya-Yaqub@users.noreply.github.com>
2023-11-30 10:36:55 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 function search() {
     const searchTerm = document.getElementById('searchInput').value.toLowerCase();
-  
-    if (searchTerm === 'coats') {
+    const coats = ['coat', 'jacket', 'parka', 'puffer', 'trench', 'windbreaker'];
+    const hats = ['hat', 'cap', 'beanie', 'headgear', 'headwear'];
+
+    if (coats.some(coat => searchTerm.includes(coat))) {
         const coatsSection = document.getElementById('Coats');
-        coatsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    } 
+        coatsSection.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+    } else if (hats.some(hat => searchTerm.includes(hat))) {
+        const hatsSection = document.getElementById('Hats');
+        hatsSection.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+    }
+
 }
+
+
+;
