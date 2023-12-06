a5f0de463ac079dae2eeb0fdbf97aae835d9b40c
Zakariya-Yaqub <148327192+Zakariya-Yaqub@users.noreply.github.com>
2023-12-06 13:33:56 +0000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<!Doctype html>
+<footer>
+    <a class="socialmedia" href="https://www.instagram.com/" target="_blank">
+        <ion-icon size="large" name="logo-instagram"></ion-icon>
+    </a>
+    <a class="socialmedia" href="https://twitter.com/" target="_blank">
+        <ion-icon size="large" name="logo-twitter"></ion-icon>
+    </a>
+</footer>
---
+++ navbar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<div class="topnav">
+        <div id="searchResults"></div>
+        <a href="aboutus.html">About Us
+            <ion-icon name="person-circle-outline"></ion-icon>
+        </a>
+        <a href="aboutus.html">Log In
+            <ion-icon name="log-in-outline"></ion-icon>
+        </a>
+        <a href="basket.html">View Basket
+            <ion-icon name="basket-outline"></ion-icon>
+        </a>
+        <a href="contactus.html">Contact Us
+            <ion-icon name="call-outline"></ion-icon>
+        </a>
+        <a href="products.html">Products
+            <ion-icon name="shirt-outline"></ion-icon>
+        </a>
+        <a class="active" href="home.html">Home
+            <ion-icon name="home"></ion-icon>
+        </a>
+        <div class="imgtopnav"></div>
+        <div class="glacier-guys">Glacier Guys</div> 
+        <div class="subtitle">"Stay Warm, Embrace Style: Elevating Winter for the Modern Man!"</div>
+        <img src="images/Glacier Guys.png" alt="pic">
+        <div class="search-container">
+            <input type="text" id="searchInput" placeholder="Coats, Hats...">
+            <button class="custom-button" onclick="search()">Search</button>
+        </div>
+
+
+</div>
