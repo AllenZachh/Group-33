3fb006286700a52d0e4ab8321384844ace3d8b37
Awais <125751393+lamborunover@users.noreply.github.com>
2023-12-06 09:59:54 +0000

    Add files via upload
    added files for the product page and list needs php to be added

--- product_page.php
+++ product_page.php
@@ -1,27 +1,9 @@
-<?php
-
-    session_start();
-    require_once('connectdb.php');
-
-    // Quieries the database using the html tag, then stores information regarding the item selected
-    $product = htmlspecialchars($_GET["select_product"]);
-    $query="SELECT * FROM product WHERE productid = $product";
-        $rows = $db->query($query);
-        $product = $rows->fetch();
-    $query="SELECT * FROM producttype WHERE productTypeid = ".$product["productTypeid"];
-        $rows = $db->query($query);
-        $productType = $rows->fetch();
-    $query = "SELECT colour, imageFilePath FROM product WHERE productTypeid = ".$productType["productTypeid"];
-        $rows = $db->query($query);
-        $differentColours = $rows->fetchAll();
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="styleAwais.css">
     <title>Document</title>
 </head>
 <body>
@@ -31,8 +13,8 @@
     </div>
     <div class="navigation">
     <ul>
-        <li><a href="home.php">Home</a></li>
-        <li><a href="products.html">Products</a></li>
+        <li><a href="home.html">Home</a></li>
+        <li><a href="products_list.html">Products</a></li>
         <li><a href="contact.html">Contact Us</a></li>
         <li><a href="about.asp">About Us</a></li>
         <div class="navright">
@@ -48,36 +30,35 @@
       
       
 <div class="productcontainer">
-    <div class="pcbox">
-        <div class="pcbrow">
             <div class ="productimage">
-                <img src = "test.jpg">
-                <!--    <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
-                  
-                    <img id="expandedImg" style >  
-                -->          
-                </div>
-            <div class = "productinfo">
-            <h2 class="pname"><?php echo $productType['name'];  ?></h2>
-            <h2 class="pprice"><?php echo $productType['price'];  ?></h3>
-            <h5 class="pdesc"><?php echo $productType['description'];  ?></h5>
-            <h3>Select your color style:</h3>
-            <div  class = "colors">
-                <?php 
-                    
-                    // Displays one of each colour of the chosen piece of clothing
-                    $displayedColours = array();
-                    foreach ($differentColours as $colour){
-                        if (!in_array($colour['colour'], $displayedColours)){
-                            echo '  <input type="radio" id="'.$colour["colour"].'" name="colors" value="'.$colour["colour"].'" >
-                                    <label for="'.$colour["colour"].'">
-                                        <img src = "'.$colour["imageFilePath"].'" onclick="expandImg(this);"/>
-                                    </label><br>';
-                        }
-                        array_push($displayedColours, $colour['colour']);
-                    }
+                   <img id="expandedImg" src = "purple.jpg"> 
+            </div>
 
-                ?>
+            <div class = "productinfo">
+                <form action="/basket.php" method="post"></form>
+            <h2 id=""  class="pname"> <?php $product[name]?> </h2>
+
+            <h2 id = " " class="pprice"><?php $product[price]?></h3>
+            <h5 id = "productid" class="productid"></h3>
+            <h5 id = " " class="pdesc"><?php $producttype[description]?><</h5>
+            <h3>Select your color style:</h3>
+            <div  id= "colors" class = "colors">     
+                <input type="radio" id="purple" name="colors" value="purple">
+                <label for="purple">
+                    <img src = "purple.jpg" onclick="expandImg(this);"/>
+                </label><br>
+                <input type="radio" id="orange" name="colors" value="orange">
+                <label for="orange">
+                        <img src = "orange.jpg"  onclick="expandImg(this);"/>
+                    </label><br>
+                <input type="radio" id="grey" name="colors" value="grey">
+                <label for="grey">
+                        <img src = "grey.jpg" onclick="expandImg(this);"/>
+                    </label><br>
+                <input type="radio" id="blue" name="colors" value="blue">
+                <label for="blue">
+                        <img src = "blue.jpg" onclick="expandImg(this);"/>
+                    </label><br>
 
 </div>
 <h3>Select your size:</h3>
@@ -96,24 +77,16 @@
 </div>
 <label for="quantity">Quantity</label>
 <input type="number" onclick="finalprice()" id="quantity" name="quantity" min="1" max="100">
-<script>
-function finalprice() {
-    var input = document.getElementById("quantity");
-    var temp = input.value * 70;
-    var sign = "£"
-    document.getElementById("price").innerHTML =sign.concat(temp) ; 
-    }
-
-    </script>
+</form>
 <div class = bottom>
 
 </div>
 <div class="cart">
-<a href="checkout.html">
-    <button>Add to Cart</button>
-</a>
+
+    <button  onclick = "checkradio()" class = "cartbutton">Add to Cart</button>
 </div>
 <h1 id = "price"</h1>
+<h2 id = "result"></h2>
 </div>
 </div>
 </div>
@@ -121,20 +94,62 @@ function finalprice() {
 </div>
 </div>
 
-<!--<script>
+<script>
     function finalprice() {
         var input = document.getElementById("quantity");
         var temp = input.value * 70;
-        var sign = "£"
+        var sign = "£";
         document.getElementById("price").innerHTML =sign.concat(temp) ; 
         }
+    
     
         function expandImg(image){
            var expandImg = document.getElementById("expandedImg");
            expandImg.src = image.src;
           
         }
+
+        function checkradio() {
+
+            var sizeSelect = document.getElementsByName("size");
+            var colorSelect = document.getElementsByName("colors");
+            var quantityCheck = document.getElementById("quantity");
+
+
+            var sizeSelected = false;
+            var colorSelected = false;
+
+            for (var i = 0, len = sizeSelect.length; i < len; i++) {
+                if (sizeSelect[i].checked === true) {
+                    sizeSelected = true;         
+                }
+            }
+
+            for (var j = 0, len2 = colorSelect.length; j < len2; j++) {
+                if (colorSelect[j].checked === true) {
+                    colorSelected = true;         
+                }
+            }
+
+
+
+            if (colorSelected === true && sizeSelected  === true && quantityCheck > 0){
+                var productname = document.getElementById("pname");
+                var sentence = " has been added to your basket";
+                document.getElementById("result").innerHTML = productname.concat(sentence);
+                }
+                else{
+                    var incorrect = "You need to select your style, size  and quantity for this product to add to the cart";
+                    document.getElementById("result").innerHTML = incorrect;
+
+                }
+          
+        }
+    
         </script>
-    -->
+
+<footer>
+    
+</footer>
 </body>
 </html>
---
+++ products_list.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet" href = "styleAwais.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="logo">
+        <h1>Glacier Gear</h1>
+        <img src = "logo.jpg">
+        </div>
+        <div class="navigation">
+        <ul>
+            <li><a href="home.html">Home</a></li>
+            <li><a href="products_list.html">Products</a></li>
+            <li><a href="contact.html">Contact Us</a></li>
+            <li><a href="about.asp">About Us</a></li>
+            <div class="navright">
+                <li><a href = "search.html">Search</a></li>
+                <li><a href="accountpage.html">Account</a></li>
+                <li><a href="basket.html">Basket</a></li>
+              </div>
+          </ul>
+        </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
