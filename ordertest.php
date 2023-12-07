32f60817b07bea3993bd46b7432d3136988009ed
AllenZachh <allenzach8@gmail.com>
2023-12-07 15:55:56 +0000

    Update ordertest.php
    Updated PHP file

--- ordertest.php
+++ ordertest.php
@@ -1,22 +1,35 @@
 <?php
 
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
+$servername = 'localhost';
+$db_name = 'glacier_guys';
+$username = 'root';
+$password = '';
 
-$conn = new mysqli_connect($servername, $username, $password);
-if (!$conn) {
-    die('connection failed'. mysql_error());
+  # Attemp to connect to database, otherwise catch and report error
+  try{
+    $db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password);
+} catch(PDOException $ex){
+    return("Failed to connect to the database.<br>");
+    //echo($ex->getMessage());
 }
-echo 'connected';
-
-$sql = SELECT productTypeid, name , price , description FROM producttype;
-$result = $conn->query($sql);
 
 
-$conn->close();
+$sql = "SELECT * FROM orderitems WHERE orderid = 1";
+$result = $db -> query ($sql) ;
+$result = $result -> fetchAll();
+
+foreach ($result as $row ) {
+    
+    $sql = $db -> prepare("UPDATE product SET stock = stock - ".$row['quantity']." WHERE productid = ".$row['productid']);
+    $sql -> execute();
+}
+
+
+
+
 ?>
 
 ?>
+
 
 
