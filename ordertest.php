5581c4787c1fff2a68bafab7c845978a43e455e1
Jacob Woodhouse <126601871+JacobW0104@users.noreply.github.com>
2023-12-07 16:37:36 +0000

    Update ordertest.php

--- ordertest.php
+++ ordertest.php
@@ -1,18 +1,6 @@
 <?php
 
-$servername = 'localhost';
-$db_name = 'glacier_guys';
-$username = 'root';
-$password = '';
-
-  # Attemp to connect to database, otherwise catch and report error
-  try{
-    $db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password);
-} catch(PDOException $ex){
-    return("Failed to connect to the database.<br>");
-    //echo($ex->getMessage());
-}
-
+require_once('connectdb.php');
 
 $sql = "SELECT * FROM orderitems WHERE orderid = 1";
 $result = $db -> query ($sql) ;
@@ -24,12 +12,4 @@ foreach ($result as $row ) {
     $sql -> execute();
 }
 
-
-
-
 ?>
-
-?>
-
-
-
