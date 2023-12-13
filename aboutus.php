1285094c7e563aa2c85ab57438d4f904f749da91
Hetmali <148331106+Hetmali@users.noreply.github.com>
2023-12-13 10:02:48 +0000

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -14,7 +14,7 @@
     <ul class="navigation">
         <li><a href="home.php">Home</a></li>
         <li><a href="products_list.php">Products</a></li>
-        <li><a href="contact.php">Contact Us</a></li>
+        <li><a href="contactus.php">Contact Us</a></li>
         <li><a href="aboutus.php">About Us</a></li>
         <li><img src="Images/Glacier Guys.png" alt="Logo" class="logo"></li>
         
