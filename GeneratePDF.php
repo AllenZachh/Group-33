99844e94ae52a4668b46bffc7e6482875ceed2ad
AllenZachh <126778035+AllenZachh@users.noreply.github.com>
2024-03-16 22:36:51 +0000

    Update GeneratePDF.php

--- GeneratePDF.php
+++ GeneratePDF.php
@@ -12,3 +12,23 @@ $password = '';
     return("Failed to connect to the database.<br>");
     //echo($ex->getMessage());
 }
+require_once 'FPDF/fpdf.php';
+require_once 'connection.php';
+$sql = //select row from table;
+$data = mysqli_query($con, $sql);
+
+if(isset($_POST['btn_pdf']))
+
+{
+  $pdf = new FPDF('p','mm','a4');
+  $pdf -> SetFont('arial', 'b', '12');
+  $pdf -> cell('40','10','Product','1','0','C');
+
+  while ($row = mysqli_fetch_assoc($data))
+
+  {
+    $pdf -> cell('40','10',row['Product'],'1','0','C');
+  }
+
+  $pdf -> Output();
+}
