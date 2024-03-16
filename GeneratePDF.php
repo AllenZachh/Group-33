a8eacb830cbe1ebfe5bca3b8e87c6c616e54da11
AllenZachh <126778035+AllenZachh@users.noreply.github.com>
2024-03-16 14:40:54 +0000

    Create GeneratePDF.php
    Generate a PDF report

---
+++ GeneratePDF.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = 'localhost';
+$db_name = 'glacier_guys';
+$username = 'root';
+$password = '';
+
+  # Attemp to connect to database, otherwise catch and report error
+  try{
+    $db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password);
+} catch(PDOException $ex){
+    return("Failed to connect to the database.<br>");
+    //echo($ex->getMessage());
+}
