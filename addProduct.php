913a2fc5818e29a4ad877deb05210bc2c5785a99
Jacob Woodhouse <126601871+JacobW0104@users.noreply.github.com>
2024-03-18 23:28:42 +0000

    Update addProduct.php

--- addProduct.php
+++ addProduct.php
@@ -2,11 +2,11 @@
 
 require_once("connectdb.php");
 
-//$stat = $db->prepare('INSERT INTO `product` (`productid`, `productTypeid`, `stock`, `colour`, `size`, `imageFilePath`) VALUES (NULL, ?, ?, ?, ?, ?)');
-            $productid = 5;
+$stat = $db->prepare('INSERT INTO `product` (`productid`, `productTypeid`, `stock`, `colour`, `size`, `imageFilePath`) VALUES (NULL, ?, ?, ?, ?, ?)');
+            $productid = 7;
             $stock = 50;
-            $colour = 'Yellow';
-            $imgFilePath = './images/Coat5Yellow.png';
+            $colour = 'Orange';
+            $imgFilePath = './images/Hat3Orange.png';
 			  $stat->execute(array($productid, $stock, $colour, "XS", $imgFilePath));
               $stat->execute(array($productid, $stock, $colour, "S", $imgFilePath));
               $stat->execute(array($productid, $stock, $colour, "M", $imgFilePath));
