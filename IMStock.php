23aa58aec4385b7503122c9a759ab3f1e46b0838
Jacob Woodhouse <126601871+JacobW0104@users.noreply.github.com>
2024-03-19 14:45:59 +0000

    IM pages .html -> .php
    IM pages only display admin controls if user is logged into an admin account

--- IMStock.php
+++ IMStock.php
@@ -1,61 +1,39 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
-<link rel="stylesheet" href="style.css">
-<script src="script.js"></script>
+<link rel="stylesheet" href="./css/style.css">
+<script src="./js/script.js"></script>
 
 <head>
     <script src="https://kit.fontawesome.com/de49bed1ab.js" crossorigin="anonymous"></script>
+    <?php require_once("navbar.php"); navbar("stockManage"); ?>
 </head>
 
 <body>
+<?php
+    if (isset($_SESSION['accountType'])){
+        if ($_SESSION['accountType'] == "admin"){
+            echo <<< EOT
     <button onclick="scrollToTop()" id="scrollToTopBtn" title="Go to top">Top</button>
 
-    <div class="topnav">
-        <div id="searchResults"></div>
-        <a href="aboutus.html">About Us
-            <ion-icon name="person-circle-outline"></ion-icon>
-        </a>
-        <a href="aboutus.html">Log In
-            <ion-icon name="log-in-outline"></ion-icon>
-        </a>
-        <a href="basket.html">View Basket
-            <ion-icon name="basket-outline"></ion-icon>
-        </a>
-        <a href="contactus.html">Contact Us
-            <ion-icon name="call-outline"></ion-icon>
-        </a>
-        <a href="products.html">Products
-            <ion-icon name="shirt-outline"></ion-icon>
-        </a>
-        <a class="active" href="IMProducts.html">Inventory Management
-            <ion-icon name="bag-outline"></ion-icon>
-        </a>
-        <a href="home.html">Home
-            <ion-icon name="home"></ion-icon>
-        </a>
-    </div>
     
     <div class="sidebar">
         <nav>
         <ul>
-            <li>
-                <a href="home.html" class="logo">
-                    <img src="images/Glacier Guys.png" alt="pic">
-                    <span class="nav-item">Glacier Guys</span>
-                </a>
-            </li>
-            <li><a href="IMProducts.html">
+            <li><a href="IMProducts.php">
               <i class="fas fa-shirt"></i>
               <span class="nav-item">Products</span>
             </a></li>
-            <li><a href="IMReport.html">
+            <li><a href="IMReport.php">
                 <i class="fas fa-newspaper"></i>
                 <span class="nav-item">Report</span> 
             </a></li>
-            <li><a href="ImStock.html">
+            <li><a href="ImStock.php">
                 <i class="fas fa-warehouse"></i>
                 <span>Stock</span>
             </a></li>
-            <li><a href="IMAnalytics.html">
+            <li><a href="IMAnalytics.php">
                 <i class="fas fa-chart-bar"></i>
                 <span>Analytics</span>
             </a>
@@ -90,3 +68,10 @@
 </body>
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+EOT;}} else {
+    echo <<< EOT
+
+    <h1>Sorry Nothing Here!!!</h1>
+
+EOT;
+}
