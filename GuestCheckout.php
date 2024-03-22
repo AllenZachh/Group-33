ba053755db9ab10e830cd5e570b4af8b7a7be3a0
y0u-suf <148952502+y0u-suf@users.noreply.github.com>
2024-03-22 17:48:44 +0000

    Update GuestCheckout.php

--- GuestCheckout.php
+++ GuestCheckout.php
@@ -1,8 +1,6 @@
 <?php
-
     session_start();
     require_once('connectdb.php');
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Checkout | Glacier Guys</title>
+    <title>Guest Checkout | Glacier Guys</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./css/style.css">
 </head>
@@ -20,6 +18,7 @@
     <div class="container mt-5">
         <div class="row">
 
+            <!-- Delivery Options -->
             <div class="col-md-6 mb-4">
                 <div class="card">
                     <div class="card-header bg-white">
@@ -52,7 +51,7 @@
                 </div>
             </div>
 
-
+            <!-- Order Summary -->
             <div class="col-md-6 mb-4">
                 <div class="card">
                     <div class="card-header bg-white">
@@ -77,7 +76,7 @@
         </div>
     </div>
 
-
+    <!-- Bootstrap's JavaScript, jQuery and Popper.js -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
